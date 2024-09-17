7ada0bf77648269d83d88db3ed352b637fced4ce
Raul Malagarriga <raulmalagarriga@hotmail.com>
2024-09-17 19:13:09 -0400

    FEAT: Updaitng body measurements

--- src/models/client.models.ts
+++ src/models/client.models.ts
@@ -4,14 +4,37 @@ import mongoose, {Schema, Document, Types} from "mongoose";
 export interface BodyMeasurements {
     date: Date,
     weight: number,
-    weightMetric: string,
-    waist: number
+    weightMetric: string, // Kg or Lbs
+    longitudeMetric: string, // Cm or inch
+    waistAnchor: number, // cintura en la parte mas ancha
+    waistNarrow: number, // cintura en la parte mas angosta
+    neck: number, // cuello
+    rightArm: number, // brazo der
+    leftArm: number, // brazo izq
+    ribCageWitShoulders: number, // Caja torácica tomando en cuenta hombros
+    ribCageWithoutShoulders: number, // Caja torácica sin hombros
+    buttocks: number, // Gluteos
+    rightThigh: number // Muslo derecho
+    leftThigh: number, // Muslo izquierdo
+    rightCalf: number // Pantorrilla derecho
+    leftCalf: number, // Pantorrilla izquierdo
 }
 const BodyMeasurementsSchema: Schema = new Schema({
     date: {type: Date, default: Date.now},
     weight: {type: Number, required: true},
     weightMetric: {type: Number, required: true},
-    waist: {type: Number, required: true}
+    waistAnchor: {type: Number, required: true},
+    waistNarrow: {type: Number, required: true},
+    neck: {type: Number, required: true},
+    rightArm: {type: Number, required: true},
+    leftArm: {type: Number, required: true},
+    ribCageWitShoulders: {type: Number, required: true},
+    ribCageWithoutShoulders: {type: Number, required: true},
+    buttocks: {type: Number, required: true},
+    rightThigh: {type: Number, required: true},
+    leftThigh: {type: Number, required: true},
+    rightCalf: {type: Number, required: true},
+    leftCalf: {type: Number, required: true},
 }, {_id: false});
 
 export interface IClient extends Document{
